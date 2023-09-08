81e6f7135124809a8bad8502939f38be35708ce2
yulun-cd <linyulun0620@gmail.com>
2023-09-08 12:04:04 +0100

    updated docstrings

--- app/core/tests/test_command.py
+++ app/core/tests/test_command.py
@@ -12,10 +12,10 @@ from django.test import SimpleTestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-    # Test commands
+    '''Test commands'''
 
     def test_wait_for_db_ready(self, patched_check):
-        # Test waiting for db if database ready
+        '''Test waiting for db if database ready'''
         patched_check.return_value = True
 
         call_command('wait_for_db')
@@ -24,7 +24,7 @@ class CommandTests(SimpleTestCase):
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-        # Test waiting for db when getting OperationalError
+        '''Test waiting for db when getting OperationalError'''
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,10 +6,10 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
-    # Test models
+    '''Test models'''
 
     def test_create_user_with_email_successful(self):
-        # Test creating a new user with an email is successful
+        '''Test creating a new user with an email is successful'''
         email = 'test@example.com'
         password = 'Testpass123'
         user = get_user_model().objects.create_user(
