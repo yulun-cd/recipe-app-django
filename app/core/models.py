17106ec68ff3f88eeca475eb5783ddb2241d278d
yulun-cd <linyulun0620@gmail.com>
2023-09-18 13:31:45 +0100

    rerecodechecker 80

--- app/core/models.py
+++ app/core/models.py
@@ -19,9 +19,9 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, email, password):
         '''Create, save and return a new superuser.'''
-        user = self.create_user(email, password, **extra_fields)
+        user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
