9929c68171a9f493356ad5525610174a260ac76a
yulun-cd <linyulun0620@gmail.com>
2023-09-22 14:07:48 +0100

    implemented recipe detail API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -9,8 +9,14 @@ from core.models import Recipe
 class RecipeSerializer(serializers.ModelSerializer):
     '''Serializer for recipes.'''
 
-
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    '''Serializer for recipe detail view.'''
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
