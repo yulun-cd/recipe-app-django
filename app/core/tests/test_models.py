302edeedc8f2dbde47370af72d9ded7146a7d3e8
yulun-cd <linyulun0620@gmail.com>
2023-10-06 08:24:07 +0100

    added test for generating uuid

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 '''
 Tests for models
 '''
+from unittest.mock import patch
 from decimal import Decimal
 
 from django.test import TestCase
@@ -81,3 +82,13 @@ class ModelTests(TestCase):
             user=user, name='Ingredient 1')
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        '''Test generating image path.'''
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'myimage.jpg')
+
+        expected_path = f'uploads/recipe/{uuid}.jpg'
+        self.assertEqual(file_path, expected_path)
